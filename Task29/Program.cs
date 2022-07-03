df520d157efdf3ae871288a81a52a26f6930d671
Plombeerec <107250294+Plombeerec@users.noreply.github.com>
2022-07-03 12:32:28 +0300

    Task29 is done

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая задаёт массив из 8 элементов 
+//и выводит их на экран.
+
+int[] array = {0,0,0,0,0,0,0,0};
+int i = 0 ;
+
+
+while ( i < array.Length)
+{
+    Console.Write($" etner {i} digit:");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+    i++;
+}
+
+var str = string.Join(" ", array);
+Console.WriteLine(str);
