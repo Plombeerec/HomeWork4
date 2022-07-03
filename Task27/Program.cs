14275852be389ae2389b3207d05bbe370220e21f
Plombeerec <107250294+Plombeerec@users.noreply.github.com>
2022-07-03 11:45:11 +0300

    Task27 done

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -1,2 +1,20 @@
 ﻿// Напишите программу, которая принимает 
 //на вход число и выдаёт сумму цифр в числе.
+
+int SumIntegersInNum(int number)
+{
+    int result = 0, sum = 0;
+    
+    while (number > 0)
+    {
+        result = number % 10;
+        number = number / 10;
+        sum += result;
+    }
+    return sum;
+}
+
+Console.Write("Input positiv intger number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Sum is" +  SumIntegersInNum(num));
